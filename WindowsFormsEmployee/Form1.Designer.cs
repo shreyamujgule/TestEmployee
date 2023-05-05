4d19dc40a843ac5f4b0aaae9e06bf7730673829f
shreyamujgule <121862223+shreyamujgule@users.noreply.github.com>
2023-05-05 15:29:52 +0530

    Test

---
+++ WindowsFormsEmployee/Form1.Designer.cs
@@ -0,0 +1,203 @@
+﻿namespace WindowsFormsEmployee
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblid = new System.Windows.Forms.Label();
+            this.txtid = new System.Windows.Forms.TextBox();
+            this.lblname = new System.Windows.Forms.Label();
+            this.txtname = new System.Windows.Forms.TextBox();
+            this.lblcity = new System.Windows.Forms.Label();
+            this.txtcity = new System.Windows.Forms.TextBox();
+            this.lbldpt = new System.Windows.Forms.Label();
+            this.txtdpt = new System.Windows.Forms.TextBox();
+            this.btnsave = new System.Windows.Forms.Button();
+            this.btndelete = new System.Windows.Forms.Button();
+            this.btnupdate = new System.Windows.Forms.Button();
+            this.lbltxt = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblid
+            // 
+            this.lblid.AutoSize = true;
+            this.lblid.Location = new System.Drawing.Point(38, 48);
+            this.lblid.Name = "lblid";
+            this.lblid.Size = new System.Drawing.Size(96, 20);
+            this.lblid.TabIndex = 0;
+            this.lblid.Text = "EmployeeID";
+            // 
+            // txtid
+            // 
+            this.txtid.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtid.Location = new System.Drawing.Point(224, 45);
+            this.txtid.Name = "txtid";
+            this.txtid.Size = new System.Drawing.Size(125, 26);
+            this.txtid.TabIndex = 1;
+            // 
+            // lblname
+            // 
+            this.lblname.AutoSize = true;
+            this.lblname.Location = new System.Drawing.Point(38, 101);
+            this.lblname.Name = "lblname";
+            this.lblname.Size = new System.Drawing.Size(121, 20);
+            this.lblname.TabIndex = 2;
+            this.lblname.Text = "EmployeeName";
+            // 
+            // txtname
+            // 
+            this.txtname.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtname.Location = new System.Drawing.Point(224, 101);
+            this.txtname.Name = "txtname";
+            this.txtname.Size = new System.Drawing.Size(246, 26);
+            this.txtname.TabIndex = 3;
+            // 
+            // lblcity
+            // 
+            this.lblcity.AutoSize = true;
+            this.lblcity.Location = new System.Drawing.Point(42, 154);
+            this.lblcity.Name = "lblcity";
+            this.lblcity.Size = new System.Drawing.Size(35, 20);
+            this.lblcity.TabIndex = 4;
+            this.lblcity.Text = "City";
+            // 
+            // txtcity
+            // 
+            this.txtcity.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtcity.Location = new System.Drawing.Point(224, 154);
+            this.txtcity.Name = "txtcity";
+            this.txtcity.Size = new System.Drawing.Size(125, 26);
+            this.txtcity.TabIndex = 5;
+            // 
+            // lbldpt
+            // 
+            this.lbldpt.AutoSize = true;
+            this.lbldpt.Location = new System.Drawing.Point(46, 211);
+            this.lbldpt.Name = "lbldpt";
+            this.lbldpt.Size = new System.Drawing.Size(94, 20);
+            this.lbldpt.TabIndex = 6;
+            this.lbldpt.Text = "Department";
+            // 
+            // txtdpt
+            // 
+            this.txtdpt.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtdpt.Location = new System.Drawing.Point(224, 204);
+            this.txtdpt.Name = "txtdpt";
+            this.txtdpt.Size = new System.Drawing.Size(211, 26);
+            this.txtdpt.TabIndex = 7;
+            // 
+            // btnsave
+            // 
+            this.btnsave.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnsave.Location = new System.Drawing.Point(77, 275);
+            this.btnsave.Name = "btnsave";
+            this.btnsave.Size = new System.Drawing.Size(82, 33);
+            this.btnsave.TabIndex = 8;
+            this.btnsave.Text = "Save";
+            this.btnsave.UseVisualStyleBackColor = true;
+            this.btnsave.Click += new System.EventHandler(this.btnsave_Click);
+            // 
+            // btndelete
+            // 
+            this.btndelete.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btndelete.Location = new System.Drawing.Point(198, 275);
+            this.btndelete.Name = "btndelete";
+            this.btndelete.Size = new System.Drawing.Size(83, 33);
+            this.btndelete.TabIndex = 9;
+            this.btndelete.Text = " Delete";
+            this.btndelete.UseVisualStyleBackColor = true;
+            this.btndelete.Click += new System.EventHandler(this.btnnext_Click);
+            // 
+            // btnupdate
+            // 
+            this.btnupdate.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnupdate.Location = new System.Drawing.Point(318, 275);
+            this.btnupdate.Name = "btnupdate";
+            this.btnupdate.Size = new System.Drawing.Size(87, 33);
+            this.btnupdate.TabIndex = 10;
+            this.btnupdate.Text = "Update";
+            this.btnupdate.UseVisualStyleBackColor = true;
+            // 
+            // lbltxt
+            // 
+            this.lbltxt.AutoSize = true;
+            this.lbltxt.Location = new System.Drawing.Point(73, 337);
+            this.lbltxt.Name = "lbltxt";
+            this.lbltxt.Size = new System.Drawing.Size(0, 20);
+            this.lbltxt.TabIndex = 11;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(928, 654);
+            this.Controls.Add(this.lbltxt);
+            this.Controls.Add(this.btnupdate);
+            this.Controls.Add(this.btndelete);
+            this.Controls.Add(this.btnsave);
+            this.Controls.Add(this.txtdpt);
+            this.Controls.Add(this.lbldpt);
+            this.Controls.Add(this.txtcity);
+            this.Controls.Add(this.lblcity);
+            this.Controls.Add(this.txtname);
+            this.Controls.Add(this.lblname);
+            this.Controls.Add(this.txtid);
+            this.Controls.Add(this.lblid);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblid;
+        private System.Windows.Forms.TextBox txtid;
+        private System.Windows.Forms.Label lblname;
+        private System.Windows.Forms.TextBox txtname;
+        private System.Windows.Forms.Label lblcity;
+        private System.Windows.Forms.TextBox txtcity;
+        private System.Windows.Forms.Label lbldpt;
+        private System.Windows.Forms.TextBox txtdpt;
+        private System.Windows.Forms.Button btnsave;
+        private System.Windows.Forms.Button btndelete;
+        private System.Windows.Forms.Button btnupdate;
+        private System.Windows.Forms.Label lbltxt;
+    }
+}
+
---
+++ WindowsFormsEmployee/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsEmployee
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnsave_Click(object sender, EventArgs e)
+        {
+            DAL dal = new DAL();
+            dal.AddParameter("employeeid", txtid.Text);
+            dal.AddParameter("employeename", txtname.Text);
+            dal.AddParameter("deduction", txtcity.Text);
+            dal.AddParameter("netsalary", txtdpt.Text);
+            dal.AddParameter("action", "update");
+            
+            dal.IsProcedureCall = true;
+           
+
+        }
+
+        private void btnnext_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
